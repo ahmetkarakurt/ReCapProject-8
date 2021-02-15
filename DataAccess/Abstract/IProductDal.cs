c530057d1334da43c248ce05df9c258792a628f0
ahmetkarakurt <cx_ahmet@hotmail.com>
2021-02-15 22:29:28 +0300

    Add files via upload

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstroct
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+
+
+    }
+}
